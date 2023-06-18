45a6d478df760a6ad1ac8b52c44edffb7f4af8ac
ViraMesha <wira.mescha@gmail.com>
2023-06-18 23:01:50 +0300

    add routing

--- App.js
+++ App.js
@@ -8,12 +8,58 @@ import {
   PostsScreen,
   CreatPostsScreen,
   ProfileScreen,
+  Home,
 } from "./src/Screens/main";
 
 const MainStack = createStackNavigator();
 const Tabs = createBottomTabNavigator();
 
+const useAuth = (isAuth) => {
+  if (!isAuth) {
+    return (
+      <MainStack.Navigator initialRouteName="Registration">
+        <MainStack.Screen
+          options={{ headerShown: false }}
+          name="Registration"
+          component={RegistrationScreen}
+        />
+        <MainStack.Screen
+          options={{ headerShown: false }}
+          name="Login"
+          component={LoginScreen}
+        />
+        <MainStack.Screen
+          name="Home"
+          options={{ title: "Публікації" }}
+          component={Home}
+        />
+      </MainStack.Navigator>
+    );
+  }
+  return (
+    <Tabs.Navigator initialRouteName="Registration">
+      <Tabs.Screen
+        name="Публікації"
+        component={PostsScreen}
+        options={{ headerTitleAlign: "center" }}
+      />
+      <Tabs.Screen
+        name="Створити публікацію"
+        component={CreatPostsScreen}
+        options={{ headerTitleAlign: "center" }}
+      />
+      <Tabs.Screen
+        options={{ headerShown: false }}
+        name="Профіль"
+        component={ProfileScreen}
+      />
+    </Tabs.Navigator>
+  );
+};
+
 export default function App() {
+  const routing = useAuth(false);
+
   const [fontsLoaded] = useFonts({
     "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
     "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
@@ -24,33 +70,5 @@ export default function App() {
     return null;
   }
 
-  return (
-    <NavigationContainer>
-      <Tabs.Navigator initialRouteName="Registration">
-        <Tabs.Screen name="Публікації" component={PostsScreen} />
-        <Tabs.Screen name="Створити публікацію" component={CreatPostsScreen} />
-        <Tabs.Screen
-          options={{ headerShown: false }}
-          name="Профіль"
-          component={ProfileScreen}
-        />
-      </Tabs.Navigator>
-    </NavigationContainer>
-  );
-}
-// auth
-{
-  /* <MainStack.Navigator initialRouteName="Registration">
-<MainStack.Screen
-  options={{ headerShown: false }}
-  name="Registration"
-  component={RegistrationScreen}
-/>
-<MainStack.Screen
-  options={{ headerShown: false }}
-  name="Login"
-  component={LoginScreen}
-/>
-<MainStack.Screen name="Home" options={{title: "Публікації"}}/>
-</MainStack.Navigator> */
+  return <NavigationContainer>{routing}</NavigationContainer>;
 }
