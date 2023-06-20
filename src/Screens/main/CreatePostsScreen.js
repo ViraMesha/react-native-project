dbe2e1dfe86434be43f0ce55483d92696b637a3a
ViraMesha <wira.mescha@gmail.com>
2023-06-20 20:50:23 +0300

    add styles for CreatPostsScreen

--- src/Screens/main/CreatePostsScreen.js
+++ src/Screens/main/CreatePostsScreen.js
@@ -1,18 +1,63 @@
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Platform,
+  ScrollView,
+  Dimensions,
+} from "react-native";
 
-import { Camera } from "../../../assets/icons";
+import { Camera, Map } from "../../../assets/icons";
 
 const CreatPostsScreen = () => {
+  const windowHeight = Dimensions.get("window").height;
+
   return (
-    <View style={styles.wrapper}>
-      <View style={styles.innerWrapper}>
-        <View style={styles.block}>
-          <TouchableOpacity activeOpacity={0.7}>
-            <Camera />
-          </TouchableOpacity>
-        </View>
-      </View>
+    <View style={styles.container}>
+      <ScrollView
+        contentContainerStyle={[
+          styles.scrollViewContentContainer,
+          { height: windowHeight - 250 },
+        ]}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={styles.contentWrapper}
+          >
+            <View style={styles.imageContainer}>
+              <TouchableOpacity activeOpacity={0.7}>
+                <Camera />
+              </TouchableOpacity>
+            </View>
+            <Text style={styles.loadPhoto}>Завантажте фото</Text>
+            <View style={styles.form}>
+              <TextInput
+                style={styles.firstInput}
+                placeholder="Назва..."
+                placeholderTextColor="#BDBDBD"
+              />
+              <View style={styles.inputContainer}>
+                <Map style={styles.inputIcon} />
+                <TextInput
+                  style={styles.secondInput}
+                  placeholder="Місцевість..."
+                  placeholderTextColor="#BDBDBD"
+                />
+              </View>
+            </View>
+            <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+              <Text style={styles.buttonText}>Опубліковати</Text>
+            </TouchableOpacity>
+          </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
+      </ScrollView>
     </View>
   );
 };
@@ -20,19 +65,23 @@ const CreatPostsScreen = () => {
 export default CreatPostsScreen;
 
 const styles = StyleSheet.create({
-  wrapper: {
-    height: "100%",
+  container: {
+    flex: 1,
     backgroundColor: "#FFFFFF",
     borderTopColor: "#BDBDBD",
     borderStyle: "solid",
     borderWidth: 1,
   },
-  innerWrapper: {
+  scrollViewContentContainer: {
+    flexGrow: 1,
+  },
+  contentWrapper: {
+    flex: 1,
     paddingTop: 32,
     paddingLeft: 16,
     paddingRight: 16,
   },
-  block: {
+  imageContainer: {
     width: 370,
     height: 240,
     borderWidth: 1,
@@ -42,5 +91,58 @@ const styles = StyleSheet.create({
     backgroundColor: "#F6F6F6",
     justifyContent: "center",
     alignItems: "center",
+  },
+  loadPhoto: {
+    marginTop: 8,
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#BDBDBD",
+  },
+  form: {
+    marginTop: 48,
+    marginBottom: 32,
+  },
+  firstInput: {
+    borderBottomWidth: 1,
+    borderStyle: "solid",
+    borderBottomColor: "#E8E8E8",
+    paddingBottom: 15,
+    fontFamily: "Roboto-Medium",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#212121",
+    marginBottom: 32,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderStyle: "solid",
+    borderBottomColor: "#E8E8E8",
+    paddingBottom: 15,
+  },
+  inputIcon: {
+    marginRight: 8,
+  },
+  secondInput: {
+    flex: 1,
+    fontFamily: "Roboto-Medium",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#212121",
+  },
+  button: {
+    backgroundColor: "#F6F6F6",
+    borderRadius: 100,
+    height: 50,
+    padding: 16,
+  },
+  buttonText: {
+    fontFamily: "Roboto-Regular",
+    textAlign: "center",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#BDBDBD",
   },
 });
